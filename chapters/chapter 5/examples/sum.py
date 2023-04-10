aa6bbb9bf66a7057ae4f82eb55bf25622bd2361c
Karen <106056574+Karen-Najafzadeh@users.noreply.github.com>
2023-04-10 23:36:17 +0330

    Create sum.py

---
+++ chapters/chapter 5/examples/sum.py
@@ -0,0 +1,19 @@
+def sum (*numbers):
+    summation = 0
+    for adad in numbers:
+        summation += adad
+    return summation
+
+
+
+def test():
+    number = float(input('give me a number\n'))
+    return number
+
+
+def add (number=float(input('give me the second number\n'))):
+    print(number*test())
+
+
+
+add()
